c000f68d24f12af94e8e910968a4a999c5182369
Haris Smajic <haris.smajic@smartsoftware.local>
2022-02-03 09:28:49 +0100

    initial commit

---
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System;
+using Microsoft.ApplicationInsights;
+
+
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        IMusicStoreEntities storeDB = null;
+        //
+        // GET: /Home/
+        public HomeController()
+        {
+            storeDB =  new MusicStoreEntities();
+        }
+
+        public HomeController(IMusicStoreEntities dbcontext)
+        {
+            storeDB = dbcontext;
+        }
+
+        public ActionResult Index()
+        {
+            // Get most popular albums
+            var albums = GetTopSellingAlbums(5);
+  
+            return View(albums);
+        }
+
+       
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+     
+
+    }
+
+   
+}
---
+++ MvcMusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreEntities : DbContext, IMusicStoreEntities
+    {
+        public MusicStoreEntities()
+        {
+            var connectionstringFromEnvironment = System.Environment.GetEnvironmentVariable(this.GetType().Name);
+            if (!string.IsNullOrEmpty(connectionstringFromEnvironment))
+            {
+                this.Database.Connection.ConnectionString = connectionstringFromEnvironment;
+            }
+        }
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<FeatureToggle> FeatureToggles { get; set; }
+        // New DB Schema
+        // Feature flag, set in context initializer
+        public static bool IsDBSchemaAlreadyAvailable { get; set; }
+        //public virtual DbSet<Product> Products { get; set; }
+        //public virtual DbSet<ProductCategory> Categories { get; set; }
+
+
+    }
+}
